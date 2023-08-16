c99ee4bc42624016e0d56d88b639ef2f8901dbdb
Mister G <108514442+Sevencito07@users.noreply.github.com>
2023-08-16 16:50:48 -0500

    Update firebase.js

--- src/componets/firebase.js
+++ src/componets/firebase.js
@@ -4,12 +4,7 @@ import { getAuth } from "@firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcnn4jm_Jk3MHiYV2akkGCS7IkA9EOHn0",
-  authDomain: "login-app-52fa8.firebaseapp.com",
-  projectId: "login-app-52fa8",
-  storageBucket: "login-app-52fa8.appspot.com",
-  messagingSenderId: "483082908179",
-  appId: "1:483082908179:web:4355904541b80d763b05e6"
+
 };
 
 // Initialize Firebase
